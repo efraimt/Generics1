50efb5241fd24002d5940dd55a9c8939ad3949cc
efraimt <efraimt@gmail.com>
2021-08-12 13:12:46 +0300

    Version 1

--- Generics1/TempRecord.cs
+++ Generics1/TempRecord.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Generics1
 {
@@ -13,7 +14,7 @@ namespace Generics1
         public float CelciusDegree { get; set; }
     }
 
-    public class Students
+    public class Student
     {
         public int Id { get; set; }
         public int Height { get; set; }
@@ -21,33 +22,76 @@ namespace Generics1
         public string Name { get; set; }
     }
 
-    public class Student
-    { 
-    
-    }
-
-
-    public class TempRecord
+    public class StudentsManagament
     {
-        // Array of temperature values
-        float[] temps = new float[10]
-        {
-        56.2F, 56.7F, 56.5F, 56.9F, 58.8F,
-        61.3F, 65.9F, 62.1F, 59.2F, 57.5F
-        };
+        public int Count { get; set; }
+        Student[] Students { get; set; } = new Student[100];
 
-        // To enable client code to validate input
-        // when accessing your indexer.
-        public int Length => temps.Length;
-
-        // Indexer declaration.
-        // If index is out of range, the temps array will throw the exception.
-        public float this[int index]
+        public void Add(Student student)
         {
-            get => temps[index];
-            set => temps[index] = value;
+            Students[Count] = student;
+            Count++;
         }
 
-        public int MyProperty { get; set; }
+        public Student LastStudent
+        { 
+            get => Students[Count - 1];
+            set => Students[Count - 1] = value;
+        }
+
+        public Student GetByIndex(int index)
+        {
+            return Students[index];
+        }
+
+
+        public Student GetById(int id)
+        {
+            foreach (var item in Students)
+            {
+                if (item.Id == id)
+                    return item;
+            }
+            return null;
+        }
+
+        public Student GetByName(string name)
+        {
+            foreach (var item in Students)
+            {
+                if(item.Name==name)
+                return item;
+            }
+            return null;
+}
+
+
+        public Student this [int i] => Students[i];
+        public Student this [string name]=>GetByName(name);
     }
+}
+
+public class TempRecord
+{
+    // Array of temperature values
+    float[] temps = new float[10]
+    {
+        56.2F, 56.7F, 56.5F, 56.9F, 58.8F,
+        61.3F, 65.9F, 62.1F, 59.2F, 57.5F
+    };
+
+    // To enable client code to validate input
+    // when accessing your indexer.
+    public int Length => temps.Length;
+
+    // Indexer declaration.
+    // If index is out of range, the temps array will throw the exception.
+    public float this[int index]
+    {
+        get => temps[index];
+        set => temps[index] = value;
+    }
+
+    public int MyProperty { get; set; }
+}
 }
